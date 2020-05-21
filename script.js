ebc472929132712862c1e20f57eb1519044f78f0
Yashant Gyawali <yashantgyawali@gmail.com>
2020-05-21 15:09:48 +0545

    added hammer.js implementation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let downPressed = false; 
       let rightPressed = false;
+      const box = document.querySelector(".block-square");
 document.onkeydown = function(event) {
       const box = document.querySelector(".block-square");
        
@@ -23,4 +24,15 @@ document.onkeydown = function(event) {
         }
 
     };
+
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+let mc = new Hammer(box);
+
+// listen to events...
+mc.on("panright pandown", function(ev) {
+    console.log(ev.type +" gesture detected.")
+    //myElement.textContent = ev.type +" gesture detected.";
+});
 
